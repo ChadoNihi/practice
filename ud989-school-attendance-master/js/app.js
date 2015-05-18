4eeb74e9a26f07285b578ef93d3d25faf8a53211
ElectroFish <chadoishere@gmail.com>
2015-05-18 21:31:15 +0300

    Sch.att. core works

--- ud989-school-attendance-master/js/app.js
+++ ud989-school-attendance-master/js/app.js
@@ -2,6 +2,8 @@
  * attendance record if one is not found
  * within localStorage.
  */
+ var NumOfDays = 12;
+
 (function() {
     if (!localStorage.attendance) {
         console.log('Creating attendance records...');
@@ -16,31 +18,66 @@
             var name = this.innerText;
             attendance[name] = [];
 
-            for (var i = 0; i <= 11; i++) {
+            for (var i = 0; i < NumOfDays; i++) {
                 attendance[name].push(getRandom());
             }
         });
 
-        localStorage.attendance = JSON.stringify(attendance);
+        localStorage.setItem('attendance', JSON.stringify(attendance));
     }
 }());
 
 $(function() {
     var octopus = {
-
+        init: function() {
+            view.init();
+        }
     };
 
     var view = {
         init: function() {
+            this.nameColumns = $('tbody .name-col');
             this.studentRows = $('tbody .student');
-            this.numOfStRows = studentRows.length;
+            this.numOfStRows = view.studentRows.length;
+            var attendance = localStorage.getItem('attendance');
+            if ($.type(attendance) === "string") attendance = JSON.parse(attendance);
 
-            for(var i=0; i<numOfStRows; i++){
-                var currRow = studentRows[i];
-                currRow.$('td .attend-col').change(function() {
-                    currRow.$('td .missed-col').val(currRow.$('td .attend-col:checked').length));
-                }));
+            for(var i=0; i<view.numOfStRows; i++){
+                var currRow = view.studentRows[i];
+                var currChBoxes = $(currRow).find('td.attend-col>input');
+                //var missCol = $(currRow).find('td.missed-col');
+                var name = $(currRow).find(':first-child').text();
+                var misses = 0;
+
+                for (var j = 0; j < NumOfDays; j++) {
+                    var currChBox = currChBoxes[j];
+                    if (attendance[name][j]) $(currChBox).prop('checked', true);
+                    else ++misses;
+
+                    $(currChBox).change(function() {
+                        var missCol = $(this).parent().siblings('td.missed-col');
+                        if ($(this).prop('checked')) $(missCol).html( parseInt($(missCol).text())-1 );
+                        else $(missCol).html( parseInt($(missCol).text())+1 );
+                        //$(missCol).val($(currRow).find('td.attend-col:checked').length);
+                    });
+                }
+                $(currRow).find('td.missed-col').text(misses);
             }
+
+            $( window ).unload(function() {
+                var attendance = {};
+
+                for (i = 0; i < view.numOfStRows; i++) {
+                    var name = view.nameColumns[i].innerText;
+                    attendance[name] = [];
+
+                    for (var j = 0; j < NumOfDays; j++) {
+                        attendance[name].push(view.studentRows[i].$('td.attend-col')[j].is(':checked'));
+                    }
+                }
+            });
         }
     };
+
+    octopus.init();
 });
