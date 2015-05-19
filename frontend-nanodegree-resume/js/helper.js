230cbe28c3e38d7e4646294af2d7a5165c7e2594
ElectroFish <chadoishere@gmail.com>
2015-05-19 23:15:57 +0300

    stubbed

--- frontend-nanodegree-resume/js/helper.js
+++ frontend-nanodegree-resume/js/helper.js
@@ -15,6 +15,16 @@ replace the %data% placeholder text you see in them.
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
+var HTMLcontactLis = {
+  HTMLcontactGeneric: '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>',
+  HTMLmobile: '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>',
+  HTMLemail: '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>',
+  HTMLtwitter: '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>',
+  HTMLgithub: '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>',
+  HTMLblog: '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>',
+  HTMLlocation: '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>'
+};
+/*
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
@@ -22,7 +32,7 @@ var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
-
+*/
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -12,14 +12,62 @@ var model = {
 		welcomeMessage: "Hey you!.. welcome!",
 		skills: ["modern web-dev", "algorithm design", "VR refactoring", "vegan meals"],
 		biopic: "../images/fry.jpg"
-	},
-	education: {
-
-	},
+	},//---------------------------------------------------------------------------------------------
 	work: {
+		jobs: [
+			{employer: "ZZ2", title: "Junior VR uplifter", location: "Paris", dates: "2013-2015", description: "Was resposible for stuff. Was resposible for stuff. Was resposible for stuff."},
+			{employer: "H++", title: "VR architector", location: "remote", dates: "2016-2018", description: "Was resposible for stuff. Was resposible for stuff."}
+		]
+	},//---------------------------------------------------------------------------------------------
+	education: {
+		schools: [
+			{name: "IT School", location: "Sydney", degree: "Bach.", majors: ["CS","Web"], dates: 2010, url: "itschool.org"},
+			{name: "Night genius", location: "Dublin", degree: "PhD", majors: ["Robots","VR"], dates: 2014, url: "ng.org"}
+		],
+		onlineCourses: [
+			{title: "Web design", school: "New-Mex", date: 2013, url: "coursera.org"},
+			{title: "Dart for VR", school: "Google S", date: 2013, url: "udacity.com"}
+		]
+	},//---------------------------------------------------------------------------------------------
+	projects: {
+		projects: [
+			{title: "Explore the depth", dates: "2012-2013", description: "Testing with current knowledge. Fun."},
+			{title: "Circle of biases", dates: "2015-2019(hopefully)", description: "Testing with current knowledge. Fun. Testing with current knowledge. Fun. Testing with current knowledge. Fun."}
+		]
+	}
+};
+var octopus = {
+	init: function() {
+		view.init();
+	}
+};
+var view = {
+	init: function() {
+		view._initHeader();
+		view._initWork();
+		view._initProjects();
+		view._initEducation();
+	},
+
+	//'private' section
+	_initHeader: function() {
+		var $header = $('#header');
+		for(var k in HTMLcontactLis){
+			if (HTMLcontactLis.hasOwnProperty(k)) {
+
+			}
+		}
+	},
+	_initWork: function() {
 
 	},
-	projects: {
-		
+	_initProjects: function() {
+
+	},
+	_initEducation: function() {
+
 	}
-}
+};
+$(function() {
+	octopus.init();
+});
